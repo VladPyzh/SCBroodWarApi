94b6f2a5637b7f0660658f4b3e5d7d7cb61abadb
Vlad Pyzh <pyzh.vlad@gmail.com>
2024-03-24 20:42:23 +0300

    medics do the job

--- src/starterbot/MyAI/Behaviors/AttackBehavior.hpp
+++ src/starterbot/MyAI/Behaviors/AttackBehavior.hpp
@@ -259,7 +259,6 @@ struct SupportBehavior : public TreeBasedBehavior<MedicStates> {
         int medics = bb.getUnits(MedicStates::Me_IDLE).size();
         
         if (medics > 0) {
-            std::cerr << "Submitted" << std::endl;
             return QuotaRequest{ 50, medics, BWAPI::UnitTypes::Terran_Medic};
         }
         else {
@@ -279,13 +278,44 @@ struct SupportBehavior : public TreeBasedBehavior<MedicStates> {
             }
         }
         auto marines = bb.m_marines;
-        auto my_marine = marines[std::min(4 + i, (int)marines.size())];
+        auto my_marine = marines[std::min(4 + i, (int)marines.size() - 1)];
 
         return  bt::one_of({
-                bt::sequence({
-                    bt::repeat_until_success([&controller, medic, my_marine, &bb = std::as_const(bb), this]() {
-                        return controller.moveUnit(medic, my_marine->unit->getPosition());
-                    })
+                bt::if_true([medic]() {
+                    if (medic->framesSinceUpdate == 0) {
+                        return false;
+                    }
+                    else {
+                        if (medic->framesSinceUpdate >= 15) {
+                            medic->framesSinceUpdate = 0;
+                            return true;
+                        }
+                        else {
+                            medic->framesSinceUpdate++;
+                            return true;
+                        }
+
+                    }
+                }),
+                bt::if_true([&controller, medic, &bb = std::as_const(bb), this]() {
+                    std::cerr << "in Seq" << std::endl;
+                    if (medic->framesSinceUpdate >= 1) {
+                        return true;
+                    }
+                    int radius = 3 * 32;
+                    BWAPI::Unitset unitsInRadius = medic->unit->getUnitsInRadius(radius);
+
+                    for (auto unit : unitsInRadius) {
+                        if (unit->isUnderAttack() && unit->getPlayer()->isEnemy(BWAPI::Broodwar->self()) == false) {
+                            medic->unit->rightClick(unit);
+                            medic->framesSinceUpdate = 1;
+                            return true;
+                        }
+                    }
+                    return false;
+                }),
+                bt::repeat_until_success([&controller, medic, my_marine, &bb = std::as_const(bb), this]() {
+                    return controller.moveUnit(medic, my_marine->unit->getPosition());
                 }),
                 bt::fail()
             });
